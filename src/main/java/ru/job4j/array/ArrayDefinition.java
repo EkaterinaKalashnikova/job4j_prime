ebf51974ad4c6cc5dfc3ae55814cdf9b17ae1cf8
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-14 21:04:54 +0300

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,11 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ekaterina Kalashnikova";
+        names[1] = "Vyacheslav Shubin";
+        names[2] = "Alexandra Bendavid";
+        names[3] = "Marcelo Venegas";
     }
 }
 
