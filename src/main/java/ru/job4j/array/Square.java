e689a69a7f3ccde8d210dc7940488a57a9b785e1
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-15 17:39:33 +0300

    Массивы в тестах. [#226789 #216185]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -3,9 +3,7 @@ package ru.job4j.array;
 public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
-        /* заполнить массив через цикл элементами от 1 до bound возведенными в квадрат */
-        for (int i = 0; i < rst.length; i++) {
-            /* rst[i] = (int) Math.pow(i + 1, 2); */
+        for (int i = 0; i < bound; i++) {
             rst[i] = i * i;
         }
         return rst;
