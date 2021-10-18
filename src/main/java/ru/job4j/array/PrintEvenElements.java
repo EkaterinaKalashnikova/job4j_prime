9818b5899c472967f01f980f906b9a2642f610b3
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-18 18:23:09 +0300

    Обход массива с последнего элемента [#361619]

---
+++ src/main/java/ru/job4j/array/PrintEvenElements.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+public class PrintEvenElements {
+    public int[] printEvenElement(int[] numbers) {
+        int evenNums = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenNums++;
+                System.out.println(
+                        "Текущий элемент массива начиная с последнего: "
+                                + numbers[numbers.length - 1 - i]
+                );
+            }
+        }
+        int[] numbers1 = new int[evenNums];
+        int index = 0;
+        for (int i = numbers.length - 1; i > 0; i--) {
+            if (numbers[i] % 2 == 0) {
+                numbers1[index] = numbers[i];
+                index++;
+            }
+        }
+        return numbers1;
+    }
+}
