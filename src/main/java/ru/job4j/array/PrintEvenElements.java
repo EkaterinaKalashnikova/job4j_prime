f075b3d9f7b35aa0485a6b75875627d78af0a06b
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-10-19 18:24:06 +0300

    Обход массива с последнего элемента [#361619 #217658]

--- src/main/java/ru/job4j/array/PrintEvenElements.java
+++ src/main/java/ru/job4j/array/PrintEvenElements.java
@@ -2,17 +2,15 @@ package ru.job4j.array;
 
 public class PrintEvenElements {
     public int[] printEvenElement(int[] numbers) {
-        int evenNums = 0;
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                evenNums++;
+            if ((numbers.length - 1 - i) % 2 == 0) {
                 System.out.println("Текущий элемент массива начиная с последнего: " + numbers[numbers.length - 1 - i]);
             }
         }
-        int[] numbers1 = new int[evenNums];
+        int[] numbers1 = new int[numbers.length / 2];
         int index = 0;
         for (int i = numbers.length - 1; i > 0; i--) {
-            if (numbers[i] % 2 == 0) {
+            if (numbers[i] % 2 != 0) {
                 numbers1[index] = numbers[i];
                 index++;
             }
--- src/test/java/ru/job4j/array/PrintEvenElementsTest.java
+++ src/test/java/ru/job4j/array/PrintEvenElementsTest.java
@@ -10,7 +10,7 @@ public class PrintEvenElementsTest {
     public  void whenPrintEvenElement() {
         PrintEvenElements printEvenElements = new PrintEvenElements();
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        int[] expected = {14, 12, 10, 8, 6, 4, 2};
+        int[] expected = {15, 13, 11, 9, 7, 5, 3};
         int[] ints = printEvenElements.printEvenElement(numbers);
         Assert.assertThat(ints, is(expected));
     }
